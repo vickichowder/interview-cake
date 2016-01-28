d50ea2434deec150d9f1cd7601cae21172d48517
vickichowder <vechow12@gmail.com>
2016-01-28 17:34:57 -0500

    Not done, wip

---
+++ word-cloud/wordcloud.py
@@ -0,0 +1,15 @@
+lines = ['After beating the eggs, Dana read the next step:','Add milk and eggs, then add flour and sugar.']
+
+class cloud:
+    def __init__(self, data):
+        self.data = data
+        self.cloud = {}
+        self.fluffy()
+        
+    def fluffy(self):
+        for item in data:
+            
+    
+    def add(self, item):
+        for word in item:
+            
