18523b1f12a2f44bd53d9dad813b1921fcb0ac9a
vickichowder <vechow12@gmail.com>
2016-05-14 21:57:27 -0400

    Returns a high value, but not the highest in all cases...

--- cake-thief/cakethief.py
+++ cake-thief/cakethief.py
@@ -1,14 +1,30 @@
-class Vault:
-    def __init__(self, inventory_list):
-        self.inventory = inventory_list
+def max_duffel_bag_value(cakes, capacity):
+    cakes = [(weight, value) for (weight, value) in cakes if weight < capacity]
+    sorted(cakes, key=lambda cake: cake[1])
+
+    print('Number of cakes eligible for heist: {}'.format(len(cakes)))
+    remaining = capacity
+    stole = 0
+    # Grab as many most expensive ones as possible
+    for cake in cakes:
+        if cake[0] <= remaining:
+            cakes_stolen = remaining/cake[0]
+            print('Stealing {} cakes of weight {}'.format(cakes_stolen, cake[0]))
+            stolen_value = cakes_stolen * cake[1]
+            stole += stolen_value
+            remaining -= cakes_stolen * cake[0]
+            print('Remaining weight left in duffle: {}'.format(remaining))
+        else:
+            next
+    print('-----------------------')
+    print('Stole {} worth of cakes'.format(stole))
 
 
-class Heist:
-    def __init__(self, vault, duffel_cap):
-        self.vault = vault
-        self.duffel_cap = duffel_cap
+cake_tuples = [(7, 160), (3, 90), (2, 15)]
+capacity1   = 20
+capacity2 = 5
 
-        self.steal()
-
-    def steal(self):
-        
+max_duffel_bag_value(cake_tuples, capacity1)
+# return 555
+# max_duffel_bag_value(cake_tuples, capacity2)
+# return 105
